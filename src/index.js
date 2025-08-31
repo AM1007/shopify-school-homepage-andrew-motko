6c9e1aaa7cf072e33765eeb37bbaa340b8f56635
Andrew Motko <andrew.motko@gmail.com>
2025-09-01 00:25:11 +0300

    add: start modal & fix image path bugs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import './js/mobile-menu';
 import './js/hero-swiper';
-// import './js/modal';
+import './js/modal';
 // import './js/featured-swiper';
 // import './js/product';
 // import './js/scroll';
